f213d7236536f4439795955de7a71eb1e9582ac6
Tsuev <tsuevjudoka@gmail.com>
2023-02-01 00:24:05 +0300

    BismiLlah

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    VitePWA(
+      { 
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        },
+        manifest: {
+          name: 'IShophone',
+          short_name: 'IShop',
+          description: 'Покупка телефона стала дешевой и доступной',
+          theme_color: '#0d9a81',
+          background_color: "#0d9a81",
+          display: "standalone",
+          icons: [
+            {
+              src: '/src/assets/pwa-logo/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/src/assets/pwa-logo/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        } 
+      }
+    )
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
