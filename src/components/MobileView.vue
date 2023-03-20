6b7f1e1f733922f85e7a5816b51945320b135892
Tsuev Mansur <tsuevjudoka@gmail.com>
2023-03-20 23:22:52 +0300

    Add mobile and desktop

---
+++ src/components/MobileView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mobileView">
+    <div class="mobileView__content">
+      <RouterView />
+    </div>
+    <Navigation />
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { RouterView } from 'vue-router'
+import { Navigation } from '@/modules/Navigation/index';
+</script>
+
+<style lang='scss' scoped>
+.mobileView {
+  @apply flex flex-col h-full;
+
+  &__content {
+    @apply grow
+  }
+}
+</style>
